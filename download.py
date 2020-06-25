ca2e2ba3eae12dc989fa7b443ba6a63ded7a5290
dqn <suyarizzz@gmail.com>
2020-06-25 18:18:12 +0900

    refactor: add NicoVideoDownloader

--- download.py
+++ download.py
@@ -1,61 +1,81 @@
-import sys
 import os
 import requests
+import sys
 import urllib.parse
+
 from bs4 import BeautifulSoup
 
 VIDEOS_DIR = './videos'
 
+class NicoVideoDownloaderExeption(Exception):
+    pass
+
+class NicoVideoDownloader:
+    def __init__(self):
+        self.session = requests.session()
+
+    def login(self, mail, password):
+        params = { 'mail_tel': mail, 'password': password }
+        res = self.session.post('https://account.nicovideo.jp/api/v1/login', params=params)
+
+        if res.headers['x-niconico-authflag'] != '1':
+            raise NicoVideoDownloaderExeption('failed to login')
+
+    def download(self, video_id):
+        # Open the video page before downloading.
+        self.session.get(f'https://www.nicovideo.jp/watch/{video_id}')
+
+        video_info = self.__get_video_info(video_id)
+        video_url = self.__get_video_url(video_id)
+
+        print(f'downloading: {video_info["title"]}')
+
+        res = self.session.get(video_url, stream=True)
+
+        if res.status_code != 200:
+            raise NicoVideoDownloaderExeption(res.text)
+
+        filename = f'{video_info["title"]}.{video_info["movie_type"]}'
+        filepath = os.path.join(VIDEOS_DIR, filename)
+
+        with open(filepath, 'wb') as f:
+            for chunk in res.iter_content(chunk_size=1024):
+                f.write(chunk)
+
+        print('completed!')
+
+    def __get_video_info(self, video_id):
+        res = self.session.get(f'https://ext.nicovideo.jp/api/getthumbinfo/{video_id}')
+
+        if res.status_code != 200:
+            raise NicoVideoDownloaderExeption(res.text)
+
+        soup = BeautifulSoup(res.text, 'lxml')
+
+        if soup.error:
+            raise NicoVideoDownloaderExeption(res.text)
+
+        return {
+            'title': soup.title.string,
+            'movie_type': soup.movie_type.string,
+        }
+
+    def __get_video_url(self, video_id):
+        res = self.session.get(f'https://flapi.nicovideo.jp/api/getflv/{video_id}')
+        flv = dict(urllib.parse.parse_qsl(res.text))
+
+        return flv['url']
+
 def usage():
     print('Usage: python3 download.py <mail> <password> <video-id>')
 
-def login(session, mail, password):
-    url = 'https://account.nicovideo.jp/api/v1/login'
-    params = { 'mail_tel': mail, 'password': password }
-    res = session.post(url, params=params)
-
-    if res.headers['x-niconico-authflag'] != '1':
-        raise Exception('failed to login')
-
-def download(session, video_id):
-    # Open the video page before downloading.
-    session.get(f'http://www.nicovideo.jp/watch/{video_id}')
-
-    thumb_info = session.get(f'http://ext.nicovideo.jp/api/getthumbinfo/{video_id}').text
-    soup = BeautifulSoup(thumb_info, 'lxml')
-
-    if soup.error:
-        raise Exception(thumb_info)
-
-    title = soup.title.string
-    movie_type = soup.movie_type.string
-
-    flv = session.get(f'http://flapi.nicovideo.jp/api/getflv/{video_id}').text
-
-    qs_to_dict = lambda qs: dict(urllib.parse.parse_qsl(qs))
-    flv = qs_to_dict(flv)
-
-    print(f'downloading: {title}')
-
-    res = session.get(flv['url'], stream=True)
-
-    if res.status_code != 200:
-        raise Exception(res.text)
-
-    file_path = os.path.join(VIDEOS_DIR, f'{title}.{movie_type}')
-    with open(file_path, 'wb') as f:
-        for chunk in res.iter_content(chunk_size=1024):
-            f.write(chunk)
-
-    print('completed!')
-
 def run(mail, password, video_id):
-    session = requests.session()
-
-    login(session, mail, password)
+    nico = NicoVideoDownloader()
+    nico.login(mail, password)
 
     os.makedirs(VIDEOS_DIR, exist_ok=True)
-    download(session, video_id)
+
+    nico.download(video_id)
 
 def main():
     if len(sys.argv) != 4:
